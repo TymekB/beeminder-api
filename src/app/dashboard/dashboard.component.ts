9d986fb686cfb8063ee115c3a992c0960d1e1d44
tymekb <baran.tymoteusz@gmail.com>
2021-01-16 13:46:19 +0100

    added environment variables

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../environments/environment';
 
 @Component({
     selector: 'app-dashboard',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-    constructor() { }
+    constructor() {
+
+        console.log(environment.beeminderAPI);
+    }
 
     ngOnInit(): void {
     }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    beeminderUrl: 'https://www.beeminder.com/api/v1/',
+    beeminderAuthToken: 'api-key'
 };
 
 /*
