4328a9a22a0c2f6d4f4a4045adbe91753f710b27
tymekb <baran.tymoteusz@gmail.com>
2021-01-21 18:32:40 +0100

    included yesterday date in counting streak
    Took 38 minutes

--- src/app/components/goal/goal.component.ts
+++ src/app/components/goal/goal.component.ts
@@ -19,24 +19,6 @@ export class GoalComponent implements OnInit {
 
     }
 
-    countStreak(datapoints, dailyMin) {
-
-        let streak = 0;
-        console.log(this.dailyMin);
-
-        for (let i = 0; i < datapoints.length; i++) {
-            const daysDifference = moment(datapoints[i].date).diff(moment(datapoints[i + 1].date), 'days');
-
-            if (daysDifference > 1 && datapoints[i].value >= dailyMin) {
-                return ++streak;
-            }
-
-            streak++;
-        }
-
-        return streak;
-    }
-
     async ngOnInit(): Promise<void> {
         this.datapoints = await this.beeminderService.fetchGoalDatapoints(this.name).toPromise();
         this.dailyMin = await this.beeminderService.fetchGoalDailyMin(this.name).toPromise();
@@ -44,4 +26,26 @@ export class GoalComponent implements OnInit {
         this.streak = this.countStreak(this.datapoints, this.dailyMin);
     }
 
+    countStreak(datapoints, dailyMin) {
+        let streak = 0;
+
+        if(moment().diff(moment(datapoints[0].date), "days") > 1) {
+            return streak;
+        }
+
+        // checks datapoints to have exactly one day difference between each other
+
+        for (let i = 0; i < datapoints.length; i++) {
+            const daysDifference = moment(datapoints[i].date).diff(moment(datapoints[i + 1].date), 'days');
+
+            if (daysDifference > 1 || datapoints[i].value < dailyMin) {
+                datapoints[i].value >= dailyMin ? streak++ : streak;
+                break;
+            }
+
+            streak++;
+        }
+
+        return streak;
+    }
 }
